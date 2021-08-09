8cd86570b40bba39b85f0322545aa30c01fe7820
Casper Verhaar <casper.verhaar@gmail.com>
2021-08-09 22:28:31 +0200

    builder: make second test pass

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,32 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_ident = &input.ident;
+    let builder_ident = format_ident!("{}Builder", input.ident);
 
-    TokenStream::new()
+    let tokens = quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #struct_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    TokenStream::from(tokens)
 }
